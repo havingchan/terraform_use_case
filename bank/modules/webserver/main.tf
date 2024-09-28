82db4ea8db6cccdac5c0d269713c3150f1b4ad16
havingchan <61160874@qq.com>
2024-09-28 23:22:17 +0800

    add providers

--- bank/modules/webserver/main.tf
+++ bank/modules/webserver/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.4.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.56"
+    }
+  }
 }
 
 resource "aws_subnet" "web_subnet" {
